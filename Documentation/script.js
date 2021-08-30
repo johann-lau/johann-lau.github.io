0122b8c555055d28e5ce61e8b3ab2730193550d7
johann-lau <johannlau8888@gmail.com>
2021-08-30 23:34:18 +0800

    Added WIP documentation

---
+++ Documentation/script.js
@@ -0,0 +1,71 @@
+var bgcolor = localStorage.getItem('b') || "#ffffff";
+var focolor = localStorage.getItem('f') || "#000000";
+var mysheet = document.getElementById("mystyle").sheet;
+
+document.body.style.backgroundColor = bgcolor;
+document.getElementsByTagName('body')[0].style.color = focolor;
+for (var i = 1; i < 6; i++) {
+  mysheet.deleteRule(mysheet.cssRules.length - 1);
+}
+mysheet.insertRule(`
+.inverted {
+background-color: ` + focolor + `;
+color: `+ bgcolor + "}", mysheet.cssRules.length);
+mysheet.insertRule(`
+#bgc {
+width:7vw;
+height: 7vw;
+border-style: solid;
+border-color: ` + focolor + `;
+background-color: ` + bgcolor + `;
+border-radius: 1vw;
+border-width:0.5vw;
+}
+`, mysheet.cssRules.length);
+mysheet.insertRule(`
+#fgc {
+width:7vw;
+height: 7vw;
+border-style: solid;
+border-color: ` + bgcolor + `;
+background-color: ` + focolor + `;
+border-radius: 1vw;
+border-width:0.5vw;
+}
+`, mysheet.cssRules.length);
+mysheet.insertRule(`
+button {
+text-align: center;
+display: inline-block;
+margin: 0.25vw 0.5vw;
+border-style: solid;
+transition-duration: 0.4s;
+cursor: pointer;
+color: ` + focolor + `;
+border-color: ` + focolor + `;
+background-color: ` + bgcolor + `;
+}
+`, mysheet.cssRules.length);
+mysheet.insertRule(`
+button:hover {
+color: ` + bgcolor + `;
+border-color: ` + focolor + `;
+background-color: ` + focolor + `;
+}
+`, mysheet.cssRules.length);
+mysheet.insertRule(`
+.card {
+  height: 8vw;
+  border-style: solid;
+  border-radius: 0.6vw;
+  border-color: `+ focolor + `;
+  margin-top: 4vw;
+}
+`, mysheet.cssRules.length);
+mysheet.insertRule(`
+table, th, td {
+  border-style: solid;
+  border-width: 0.1vw;
+  border-color: `+ focolor + ";}", mysheet.cssRules.length);
+localStorage.setItem('b', bgcolor);
+localStorage.setItem('f', focolor);
