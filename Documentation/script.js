0bc61ab03be806ec5a9c652ce75839690104f4ff
johann-lau <johannlau8888@gmail.com>
2021-08-31 17:31:29 +0800

    Compact theme picker
    Now it would consume less battery!
    
    I still can't decide whether I should put the theme picker at the bottom or not, though…

--- Documentation/script.js
+++ Documentation/script.js
@@ -11,39 +11,20 @@ for (var i = 0; i < links.length; i++) {
 for (var i = 1; i < 6; i++) {
   mysheet.deleteRule(mysheet.cssRules.length - 1);
 }
+
 mysheet.insertRule(`
 body, a {
-  color: `+focolor+`;
+  color: `+ focolor + `;
   font-family: Font1, Arial, Helvetica, sans-serif;
   scroll-behavior: smooth;
   position: relative;
 }`, mysheet.cssRules.length)
+
 mysheet.insertRule(`
 .inverted {
 background-color: ` + focolor + `;
 color: `+ bgcolor + "}", mysheet.cssRules.length);
-mysheet.insertRule(`
-#bgc {
-width:7vw;
-height: 7vw;
-border-style: solid;
-border-color: ` + focolor + `;
-background-color: ` + bgcolor + `;
-border-radius: 1vw;
-border-width:0.5vw;
-}
-`, mysheet.cssRules.length);
-mysheet.insertRule(`
-#fgc {
-width: 7vw;
-height: 7vw;
-border-style: solid;
-border-color: ` + bgcolor + `;
-background-color: ` + focolor + `;
-border-radius: 1vw;
-border-width:0.5vw;
-}
-`, mysheet.cssRules.length);
+
 mysheet.insertRule(`
 button {
 text-align: center;
@@ -57,6 +38,7 @@ border-color: ` + focolor + `;
 background-color: ` + bgcolor + `;
 }
 `, mysheet.cssRules.length);
+
 mysheet.insertRule(`
 button:hover {
 color: ` + bgcolor + `;
@@ -64,20 +46,23 @@ border-color: ` + focolor + `;
 background-color: ` + focolor + `;
 }
 `, mysheet.cssRules.length);
+
 mysheet.insertRule(`
 .card {
-  height: 8vw;
-  border-style: solid;
-  border-radius: 0.6vw;
-  border-color: `+ focolor + `;
-  margin-top: 4vw;
+height: 8vw;
+border-style: solid;
+border-radius: 0.6vw;
+border-color: `+ focolor + `;
+margin-top: 4vw;
 }
 `, mysheet.cssRules.length);
+
 mysheet.insertRule(`
 table, th, td {
-  table-layout: fixed;
-  border-style: solid;
-  border-width: 0.1vw;
-  border-color: `+ focolor + ";}", mysheet.cssRules.length);
+table-layout: fixed;
+border-style: solid;
+border-width: 0.1vw;
+border-color: `+ focolor + ";}", mysheet.cssRules.length);
+
 localStorage.setItem('b', bgcolor);
 localStorage.setItem('f', focolor);
--- script.js
+++ script.js
@@ -3,56 +3,28 @@ var focolor = localStorage.getItem('f') || "#000000";
 var mysheet = document.getElementById("mystyle").sheet;
 blist = ["#FFFFFF", "#37393E", "#76D3F9", "#7DA8F7", "#AA90F7", "#D798F8", "#DE799D", "#F19286", "#F3A884", "#F7C783", "#FAD885", "#FFF8A2", "#EDF29B", "#BADD95"];
 flist = ["#000000", "#DCDDDE", "#1D4C63", "#0A3075", "#170F4F", "#3F1556", "#4F1729", "#791E0E", "#732F10", "#744C16", "#74581A", "#8C8529", "#717524", "#3F5623"];
-bg = document.getElementById('bgc')
-bg.addEventListener("change", BGc, false);
-fg = document.getElementById('fgc')
-fg.addEventListener("change", FGc, false);
-
-function BGc(event) {
-  bgcolor = event.target.value;
-  setcolor();
-}
-
-function FGc(event) {
-  focolor = event.target.value;
-  setcolor();
-}
 
 function setcolor() {
-  bg.value = bgcolor;
-  fg.value = focolor;
   document.body.style.backgroundColor = bgcolor;
   document.getElementsByTagName('body')[0].style.color = focolor;
   for (var i = 1; i < 6; i++) {
     mysheet.deleteRule(mysheet.cssRules.length - 1);
   }
+
+mysheet.insertRule(`
+body, a {
+  color: `+focolor+`;
+  font-family: Font1, Arial, Helvetica, sans-serif;
+  scroll-behavior: smooth;
+  position: relative;
+}`, mysheet.cssRules.length)
+
   mysheet.insertRule(`
 .inverted {
 background-color: ` + focolor + `;
 color: `+ bgcolor + "}", mysheet.cssRules.length);
-  mysheet.insertRule(`
-#bgc {
-width:7vw;
-height: 7vw;
-border-style: solid;
-border-color: ` + focolor + `;
-background-color: ` + bgcolor + `;
-border-radius: 1vw;
-border-width:0.5vw;
-}
-`, mysheet.cssRules.length);
-  mysheet.insertRule(`
-#fgc {
-width:7vw;
-height: 7vw;
-border-style: solid;
-border-color: ` + bgcolor + `;
-background-color: ` + focolor + `;
-border-radius: 1vw;
-border-width:0.5vw;
-}
-`, mysheet.cssRules.length);
-  mysheet.insertRule(`
+
+mysheet.insertRule(`
 button {
 text-align: center;
 display: inline-block;
@@ -65,6 +37,7 @@ border-color: ` + focolor + `;
 background-color: ` + bgcolor + `;
 }
 `, mysheet.cssRules.length);
+
   mysheet.insertRule(`
 button:hover {
 color: ` + bgcolor + `;
@@ -72,6 +45,7 @@ border-color: ` + focolor + `;
 background-color: ` + focolor + `;
 }
 `, mysheet.cssRules.length);
+
   mysheet.insertRule(`
 .card {
   height: 8vw;
@@ -81,6 +55,7 @@ background-color: ` + focolor + `;
   margin-top: 4vw;
 }
 `, mysheet.cssRules.length);
+
   localStorage.setItem('b', bgcolor);
   localStorage.setItem('f', focolor);
 }
